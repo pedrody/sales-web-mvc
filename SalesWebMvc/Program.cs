f58e83f06795406c73d11bb080965d101572665c
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2025-12-21 14:03:06 -0300

    feat: create SellerFormViewModel to select a department when creating a new seller

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
 
 var app = builder.Build();
 
