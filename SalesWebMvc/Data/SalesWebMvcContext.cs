6367f9cad562138f5bda1f47bd00c03d0fad7f02
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2025-12-22 00:07:04 -0300

    feat: handle exception for delete (referential integrity)

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -4,7 +4,7 @@ namespace SalesWebMvc.Data
 {
     public class SalesWebMvcContext : DbContext
     {
-        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+        public SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options)
             : base(options)
         {
         }
@@ -12,5 +12,15 @@ namespace SalesWebMvc.Data
         public DbSet<Models.Department> Department { get; set; } = default!;
         public DbSet<Models.Seller> Seller { get; set; } = default!;
         public DbSet<Models.SalesRecord> SalesRecord { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Models.SalesRecord>()
+                .HasOne(s => s.Seller)
+                .WithMany(sel => sel.Sales)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
